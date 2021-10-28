8d62c22bb82328dfe70f93b41d42d76303ac8231
JunSeo <oponize@naver.com>
2021-10-28 15:23:15 +0900

    [fix] boilerplate

---
+++ reactBoilerplate/config/webpack.dev.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const paths = require('./paths');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    historyApiFallback: true,
+    static: paths.public,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, importLoaders: 1, modules: false },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+});
